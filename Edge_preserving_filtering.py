72dffb50906cd70b2188160346b25ced773d59a1
STLLYM <167419431@qq.com>
2020-08-18 01:01:30 +0800

    data

--- Edge_preserving_filtering.py
+++ Edge_preserving_filtering.py
@@ -17,7 +17,12 @@ def shift_demo(image):
 src = cv.imread("opencv-4.4.0/data/lena.jpg")
 cv.namedWindow("the first image", cv.WINDOW_AUTOSIZE)
 cv.imshow("the first image", src)
-# bi_demo(src)
+bi_demo(src)
 # shift_demo(src)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+# 测试功能
+# 边缘保留滤波（EPF）
+# 代码编写时间 2020.8.13
+
